a709b07fc885ea1fde7709721598defabe649dab
hamusita <meazero965@gmail.com>
2019-07-30 22:29:51 +0900

    complete

---
+++ movie.py
@@ -0,0 +1,89 @@
+import sys
+import cv2 
+
+# OpenCV のインポート
+# VideoCaptureのインスタンスを作成する。
+# 引数でカメラを選べれる。
+cap = cv2.VideoCapture(0)
+
+if cap.isOpened() is False:
+    print("can not open camera")
+    sys.exit()
+
+# 評価器を読み込み
+cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
+
+def img_resize(img, h, w):
+    img = cv2.resize(img, (int(w), int(h)))
+    return img
+
+def merge_images(bg, fg_alpha, s_x, s_y):
+    alpha = fg_alpha[:,:,3]  # アルファチャンネルだけ抜き出す(要は2値のマスク画像)
+    alpha = cv2.cvtColor(alpha, cv2.COLOR_GRAY2BGR) # grayをBGRに
+    alpha = alpha / 255.0    # 0.0〜1.0の値に変換
+
+    fg = fg_alpha[:,:,:3]
+
+    f_h, f_w, _ = fg.shape # アルファ画像の高さと幅を取得
+    b_h, b_w, _ = bg.shape # 背景画像の高さを幅を取得
+
+    # 画像の大きさと開始座標を表示
+    print("f_w:{} f_h:{} b_w:{} b_h:{} s({}, {})".format(f_w, f_h, b_w, b_h, s_x, s_y))
+
+    bg[s_y:f_h+s_y, s_x:f_w+s_x] = (bg[s_y:f_h+s_y, s_x:f_w+s_x] * (1.0 - alpha)).astype('uint8') # アルファ以外の部分を黒で合成
+    bg[s_y:f_h+s_y, s_x:f_w+s_x] = (bg[s_y:f_h+s_y, s_x:f_w+s_x] + (fg * alpha)).astype('uint8')  # 合成
+
+    return bg
+
+while True:
+    # VideoCaptureから1フレーム読み込む
+    ret, frame = cap.read()
+
+    # そのままの大きさだと処理速度がきついのでリサイズ
+    frame = cv2.resize(frame, (int(frame.shape[1]*0.7), int(frame.shape[0]*0.7)))
+
+    # 処理速度を高めるために画像をグレースケールに変換したものを用意
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # 顔検出
+    facerect = cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.11,
+        minNeighbors=3,
+        minSize=(100, 100)
+    )
+
+    if len(facerect) != 0:
+        for x, y, w, h in facerect:
+            # 顔の部分(この顔の部分に対して目の検出をかける)
+            face_gray = gray[y: y + h, x: x + w]
+
+            # くり抜いた顔の部分を表示(処理には必要ない。ただ見たいだけ。)
+            show_face_gray = cv2.resize(face_gray, (int(gray.shape[1]), int(gray.shape[0])))
+            cv2.imshow('face', show_face_gray)
+
+            # 顔検出した部分に枠を描画
+            cv2.rectangle(
+                frame,
+                (x, y),
+                (x + w, y + h),
+                (255, 255, 255),
+                thickness=2
+            )
+
+            print((x, y),(x + w, y + h))
+            icon = cv2.imread("./nc73730.png", -1)
+            icon = img_resize(icon, h, w)
+            print(h,w)
+            frame = merge_images(frame, icon, x, y)
+    
+    cv2.imshow('frame', frame)
+    
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
+
+# キャプチャをリリースして、ウィンドウをすべて閉じる
+cap.release()
+cv2.destroyAllWindows()
--- test.py
+++ test.py
@@ -1,32 +1,118 @@
-import tkinter as tk
+# -*- coding: utf-8 -*-
+import cv2
+import dlib
+from datetime import datetime
 
-class Demo1:
-    def __init__(self, master):
-        self.master = master
-        self.frame = tk.Frame(self.master)
-        self.button1 = tk.Button(self.frame, text = 'New Window', width = 25, command = self.new_window)
-        self.button1.pack()
-        self.frame.pack()
+IMAGE_PATH = "./nc73730.png" # 画像パス
+CAPTURE_SCALE = 0.5 # カメラの画像サイズ
 
-    def new_window(self):
-        self.newWindow = tk.Toplevel(self.master)
-        self.app = Demo2(self.newWindow)
+def main():
+    # 顔認証の準備
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-class Demo2:
-    def __init__(self, master):
-        self.master = master
-        self.frame = tk.Frame(self.master)
-        self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
-        self.quitButton.pack()
-        self.frame.pack()
+    cap = cv2.VideoCapture(0) # カメラの準備
 
-    def close_windows(self):
-        self.master.destroy()
+    while True:
+        _, frame = cap.read() # カメラ画像を取得
+        
+        # 画像サイズを変更
+        frame = img_resize(frame, CAPTURE_SCALE)
+        h, w, _  = frame.shape
 
-def main(): 
-    root = tk.Tk()
-    app = Demo1(root)
-    root.mainloop()
+        img = frame # img = frame * 0とすると黒画面になる
+        dets = detector(frame[:, :, ::-1])
+
+        if len(dets) > 0:
+            parts = predictor(frame, dets[0]).parts() # 顔の点を取得
+            distance, pos = calc_distance_and_pos(img, parts) # 顔の横幅と顔の中心を取得
+            icon, icon_w, icon_h = load_icon(IMAGE_PATH, distance) # アイコン読み込み
+
+            if (pos != None) and (distance > 0.0):
+                x = pos.x - int(icon_w/2)
+                y = pos.y - int(icon_h/2)
+                if (0 <= y) and (y <= (h-int(icon_h))) and (0 <= x) and (x <= (w-int(icon_w))):
+                    # 画面の範囲内だったら画像を合成
+                    img = merge_images(img, icon, x, y)
+
+        cv2.imshow("camera", img) # 画像を表示
+
+        k = cv2.waitKey(1)&0xff # キー入力を待つ
+        if k == ord('q'):
+            # 「q」キーが押されたら終了する
+            break
+        elif k == ord('p'):
+            # 「p」キーで画像を保存
+            save_image(img)
+            cv2.imshow("saved", img) # キャプチャした画像を表示
+
+    # キャプチャをリリースして、ウィンドウをすべて閉じる
+    cap.release()
+    cv2.destroyAllWindows()
+
+# アイコンを読み込む関数
+def load_icon(path, distance):
+    icon = cv2.imread(path, -1)
+    icon_height, _  = icon.shape[:2]
+    icon = img_resize(icon, float(distance * 1.5/icon_height))
+    icon_h, icon_w  = icon.shape[:2]
+
+    return icon, icon_w, icon_h
+
+# 画像をリサイズする関数
+def img_resize(img, scale):
+    h, w  = img.shape[:2]
+    img = cv2.resize(img, (int(w*scale), int(h*scale)))
+    return img
+
+# 距離と顔の中心座標を計算
+def calc_distance_and_pos(img, parts):
+    # 確認(33が顔の中心位置)
+    cnt = 0
+    pos = None
+    p1 = None
+    distance = 0.0
+
+    for i in parts:
+        if (cnt == 0):
+            # 顔の幅を測る時の始点
+            p1 = i
+        if (cnt == 16):
+            # 顔の幅を計算
+            distance = ((p1.x-i.x)**2 + (p1.y-i.y)**2)**0.5
+        if (cnt == 33):
+            pos = i # 顔の中心位置
+        # 画像に点とテキストをプロット
+        cv2.putText(img, str(cnt), (i.x, i.y), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 255), thickness=1, lineType=cv2.LINE_8)
+        cv2.circle(img, (i.x, i.y), 1, (255, 0, 0), -1)
+        cnt = cnt + 1
+
+    return distance, pos
+
+# 画像を保存する関数
+def save_image(img):
+    date = datetime.now().strftime("%Y%m%d_%H%M%S")
+    path = "./" + date + ".png"
+    cv2.imwrite(path, img) # ファイル保存
+
+# 画像を合成する関数
+def merge_images(bg, fg_alpha, s_x, s_y):
+    alpha = fg_alpha[:,:,3]  # アルファチャンネルだけ抜き出す(要は2値のマスク画像)
+    alpha = cv2.cvtColor(alpha, cv2.COLOR_GRAY2BGR) # grayをBGRに
+    alpha = alpha / 255.0    # 0.0〜1.0の値に変換
+
+    fg = fg_alpha[:,:,:3]
+
+    f_h, f_w, _ = fg.shape # アルファ画像の高さと幅を取得
+    b_h, b_w, _ = bg.shape # 背景画像の高さを幅を取得
+
+    # 画像の大きさと開始座標を表示
+    print("f_w:{} f_h:{} b_w:{} b_h:{} s({}, {})".format(f_w, f_h, b_w, b_h, s_x, s_y))
+
+    bg[s_y:f_h+s_y, s_x:f_w+s_x] = (bg[s_y:f_h+s_y, s_x:f_w+s_x] * (1.0 - alpha)).astype('uint8') # アルファ以外の部分を黒で合成
+    bg[s_y:f_h+s_y, s_x:f_w+s_x] = (bg[s_y:f_h+s_y, s_x:f_w+s_x] + (fg * alpha)).astype('uint8')  # 合成
+
+    return bg
 
 if __name__ == '__main__':
     main()
